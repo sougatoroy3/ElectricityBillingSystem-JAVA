c64463d94a109c1d694b42478f665a607ddd71d9
sougatoroy3 <sougatoroy007@gmail.com>
2024-04-09 13:42:02 +0530

    Sign Up Frame Updated

--- src/electricity/billing/system/Signup.java
+++ src/electricity/billing/system/Signup.java
@@ -10,11 +10,14 @@ import javax.swing.*;
 import javax.swing.border.*; //as java doesnt import the subpackages only classes are imported
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.*;
 
 public class Signup extends JFrame implements ActionListener{
     
-    //Global Variables
+    //Global Declaration of Variables
     JButton create, back;
+    Choice accountType;
+    JTextField meter, username, name, password;
     Signup(){
         //setSize(700,400)
         //setLocation(450,150)
@@ -40,24 +43,27 @@ public class Signup extends JFrame implements ActionListener{
         panel.add(heading);//setting it over the panel and not the frame
    
         // Choice of account type
-        Choice accountType=new Choice();
+        accountType=new Choice();
         accountType.add("Admin");
         accountType.add("Customer");
         accountType.setBounds(260,50,150,20);
         panel.add(accountType);
+        
         
         //Meter number Label
         JLabel lblmeter=new JLabel("Meter Number");
         lblmeter.setBounds(100, 90, 140, 20); 
         lblmeter.setForeground(Color.GRAY);
         lblmeter.setFont(new Font("Tahoma", Font.BOLD, 14));//Tahoma is font style and 14 is font size
+        lblmeter.setVisible(false);
         panel.add(lblmeter);//setting it over the panel and not the frame
    
         //Input Field for meter number
-        JTextField meter=new JTextField();
+        meter=new JTextField();
         meter.setBounds(260, 90, 150, 20);
+        meter.setVisible(false);
         panel.add(meter);
-        
+
         //Username Label
         JLabel lblusername=new JLabel("Username");
         lblusername.setBounds(100, 130, 140, 20); 
@@ -66,7 +72,7 @@ public class Signup extends JFrame implements ActionListener{
         panel.add(lblusername);//setting it over the panel and not the frame
    
         //Input Field for Username
-        JTextField username=new JTextField();
+        username=new JTextField();
         username.setBounds(260, 130, 150, 20);
         panel.add(username);
         
@@ -78,10 +84,27 @@ public class Signup extends JFrame implements ActionListener{
         panel.add(lblname);//setting it over the panel and not the frame
    
         //Input Field for Name
-        JTextField name=new JTextField();
+        name=new JTextField();
         name.setBounds(260, 170, 150, 20);
         panel.add(name);
         
+        meter.addFocusListener(new FocusListener(){
+            public void focusGained(FocusEvent fe) {}
+            
+            @Override
+            public void focusLost(FocusEvent fe) {
+                try {
+                    Conn c  = new Conn();
+                    ResultSet rs = c.s.executeQuery("select * from login where meter_no = '"+meter.getText()+"'");
+                    while(rs.next()) {
+                        name.setText(rs.getString("name"));
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
         //Password Label
         JLabel lblpassword=new JLabel("Password");
         lblpassword.setBounds(100, 210, 140, 20); 
@@ -90,9 +113,24 @@ public class Signup extends JFrame implements ActionListener{
         panel.add(lblpassword);//setting it over the panel and not the frame
    
         //Input Field for Password
-        JTextField password=new JTextField();
+        password=new JTextField();
         password.setBounds(260, 210, 150, 20);
         panel.add(password);
+        
+        accountType.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent ae) {
+                String user = accountType.getSelectedItem();
+                if (user.equals("Customer")) {
+                    lblmeter.setVisible(true);
+                    meter.setVisible(true);
+                    name.setEditable(false);
+                } else {
+                    lblmeter.setVisible(false);
+                    meter.setVisible(false);
+                    name.setEditable(true);
+                }
+            }
+        });
         
         //Buttons
         //Create Button
@@ -123,7 +161,32 @@ public class Signup extends JFrame implements ActionListener{
     
     public void actionPerformed(ActionEvent ae){
         if(ae.getSource()==create){
+            String atype=accountType.getSelectedItem();//item is retrieved 
+            String susername=username.getText();
+            String sname=name.getText();
+            String spassword=password.getText();
+            String smeter=meter.getText();
             
+            //under try and catch block cause there may be a chance of exception
+            try{
+                Conn c=new Conn();
+                String query=null; //creating a query 
+                //inserting as per table
+                if (atype.equals("Admin")) {
+                    query = "insert into login values('"+smeter+"', '"+susername+"', '"+sname+"', '"+spassword+"', '"+atype+"')";
+                } else {
+                    query = "update login set username = '"+susername+"', password = '"+spassword+"', user = '"+atype+"' where meter_no = '"+smeter+"'";
+                }
+                c.s.executeUpdate(query);
+                
+                JOptionPane.showMessageDialog(null, "Account Created Successfully");
+                
+                setVisible(false);
+                new Login();
+            }catch(Exception e)
+            {
+                e.printStackTrace();
+            }
         }else if(ae.getSource()==back){
             setVisible(false);
             new Login();
